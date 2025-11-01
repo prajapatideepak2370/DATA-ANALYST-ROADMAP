371319ef0bdc9f5e593b9df5ab2528742184ae6e
Deepak Prajapati <prajapatideepak2370@gmail.com>
2025-11-02 00:30:47 +0530

    Add File

--- PYTHON/Basicpy/pattern.py
+++ PYTHON/Basicpy/pattern.py
@@ -1,13 +1,13 @@
 n = int(input("Enter the number: " ))
-# def pattern1(n):
-#     for i in range(1, n+1):
-#          for j in range(1, n+1):
-#             print("*", end=" ")
-#          print()
-# for i in range(n+1,-1):
-#     for j in range(n-i+1, 0, -1):
-#         print("*", end=" ")
-#     print()    
+def pattern1(n):
+    for i in range(1, n+1):
+         for j in range(1, n+1):
+            print("*", end=" ")
+         print()
+for i in range(n+1,-1):
+    for j in range(n-i+1, 0, -1):
+        print("*", end=" ")
+    print()    
 
 # print(pattern1(n))
 '''
@@ -15,10 +15,10 @@ n = int(input("Enter the number: " ))
  ***
 *****
 '''
-# for i in range(1,n+1):
-#     print(" "*(n-i), end="") #this print satement is used to print space 
-#     print("*" * (2*i - 1), end= "")
-#     print("")
+for i in range(1,n+1):
+    print(" "*(n-i), end="") #this print satement is used to print space 
+    print("*" * (2*i - 1), end= "")
+    print("")
 
 '''
 *
@@ -26,26 +26,26 @@ n = int(input("Enter the number: " ))
 ***
 '''
 
-# for i in range(1,n+1):
-#     print("*" * i, end= "")
-#     print("")
+for i in range(1,n+1):
+    print("*" * i, end= "")
+    print("")
 
 '''
 * * *
 *   *
 * * *
 '''
-# for i  in range(1,n+1):
-#     if(i==1 or i==n):
-#         print("*" * n, end="")
-#     else:
-#         print("*", end="")
-#         print(" " * (n-2), end="")
-#         print("*", end="")
-#     print("")
+for i  in range(1,n+1):
+    if(i==1 or i==n):
+        print("*" * n, end="")
+    else:
+        print("*", end="")
+        print(" " * (n-2), end="")
+        print("*", end="")
+    print("")
 
-# for i  in range(1,11):
-#     print(f"{n}X{11-i} = {n*(11-i)}")
+for i  in range(1,11):
+    print(f"{n}X{11-i} = {n*(11-i)}")
 '''
 1 
 1 2 
