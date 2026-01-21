888db974b809111770a56b68b488650eb95387e7
prajapatideepak2370 <prajapatideepak2370@gmail.com>
2026-01-21 18:01:40 +0530

    Logging

---
+++ PYTHON/Logging/log1.py
@@ -0,0 +1,16 @@
+import logging
+class DemoLogging:
+    def add_nums(self, a, b):
+        return a+b
+    def mult_nums(self, a, b):
+        return a * b
+
+dl = DemoLogging()
+sum_result = dl.add_nums(3, 5)
+logging.info("info: addition of the numbers is: {}".format(sum_result))
+logging.debug("debug: addition of the numbers is: {}".format(sum_result))
+logging.error("error: addition of the numbers is: {}".format(sum_result))
+logging.critical("critical: addition of the numbers is: {}".format(sum_result))
+logging.warning("warning: addition of the numbers is: {}".format(sum_result))
+mult_result = dl.mult_nums(3, 5)
+logging.warning("The multiplication of the number is: {}".format(mult_result))
